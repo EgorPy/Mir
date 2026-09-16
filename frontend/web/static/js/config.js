8db52237f0f2a4d207826560da7b6f15699ee229
Egor Mironov <e.mironov@shtab.studio>
2026-09-16 12:41:06 +0000

    Add new file

---
+++ frontend/web/static/js/config.js
@@ -0,0 +1,5 @@
+export const BACKEND_URL = "https://mir.shtab.digital/api";
+export const CALLS_URL = "https://mir.shtab.digital/api/ws-call";
+
+window.BACKEND_URL = BACKEND_URL;
+window.CALLS_URL = CALLS_URL;
